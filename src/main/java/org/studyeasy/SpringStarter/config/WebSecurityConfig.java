e04179d4fc9a44e20f05e24edb88fe5f09a74003
viru0909-dev <gadekarvirendra4@gmail.com>
2025-06-10 23:54:44 +0530

    just small change

--- src/main/java/org/studyeasy/SpringStarter/config/WebSecurityConfig.java
+++ src/main/java/org/studyeasy/SpringStarter/config/WebSecurityConfig.java
@@ -20,6 +20,7 @@ public class WebSecurityConfig {
       "/js/**"
     };
     @Bean
+  
     public  SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
         http
         .authorizeRequests()

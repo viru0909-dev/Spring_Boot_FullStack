a246986e1ab266233f1c6fdb946847996d6ae7ab
viru0909-dev <gadekarvirendra4@gmail.com>
2025-06-11 13:19:49 +0530

    spring security adding login page

--- src/main/java/org/studyeasy/SpringStarter/config/WebSecurityConfig.java
+++ src/main/java/org/studyeasy/SpringStarter/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -20,14 +22,32 @@ public class WebSecurityConfig {
       "/js/**"
     };
     @Bean
-  
+    public static PasswordEncoder passwordEncoder(){
+      return new BCryptPasswordEncoder(); 
+    }
+    @Bean
     public  SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
         http
         .authorizeRequests()
         .antMatchers(WHITELIST)
         .permitAll()
         .anyRequest()
-        .authenticated();
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .loginProcessingUrl("/login")
+        .usernameParameter("email")
+        .passwordParameter("password")
+        .defaultSuccessUrl("/",true)
+        .failureUrl("/login?error")
+        .permitAll()
+        .and()
+        .logout()
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/logout?success")
+        .and()
+        .httpBasic();
 
         //TODO: remove these after upgrading the DB from H2 infile DB
         http.csrf().disable();

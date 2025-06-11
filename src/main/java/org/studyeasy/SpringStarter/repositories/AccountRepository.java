1616bfe4dd0e38a28591b8c835d896ff9fd5372c
viru0909-dev <gadekarvirendra4@gmail.com>
2025-06-11 13:43:38 +0530

    Spring security login, override loadUserByUserName

--- src/main/java/org/studyeasy/SpringStarter/repositories/AccountRepository.java
+++ src/main/java/org/studyeasy/SpringStarter/repositories/AccountRepository.java
@@ -1,11 +1,15 @@
 package org.studyeasy.SpringStarter.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.studyeasy.SpringStarter.models.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
+
+    Optional<Account> findByEmailIgnoreCase(String username);
     
 }
 
--- src/main/java/org/studyeasy/SpringStarter/services/AccountService.java
+++ src/main/java/org/studyeasy/SpringStarter/services/AccountService.java
@@ -1,13 +1,23 @@
 package org.studyeasy.SpringStarter.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.studyeasy.SpringStarter.models.Account;
 import org.studyeasy.SpringStarter.repositories.AccountRepository;
 
 @Service
-public class AccountService {
+public class AccountService implements UserDetailsService {
     @Autowired
     private AccountRepository accountRepository;
 
@@ -18,6 +28,24 @@ public class AccountService {
     public Account save(Account account){
         account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accountRepository.save(account);    
+    }   
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Optional<Account> optionalAccount = accountRepository.findByEmailIgnoreCase(email);
+        if(!optionalAccount.isPresent()){
+            throw  new  UsernameNotFoundException("Account not found");
+        }
+        Account account = optionalAccount.get();
+
+
+
+        List<GrantedAuthority> grantedAuthoritys =new ArrayList<>();
+        grantedAuthoritys.add(new SimpleGrantedAuthority("Allow"));
+
+        return new User(account.getEmail(),account.getPassword(),grantedAuthoritys);
     }
     
 }
